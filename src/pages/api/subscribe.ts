803359627cdd036de1d7fb99503851568c6e3d44
Natalia Martineli <nataliamartineli@gmail.com>
2022-01-20 18:46:01 -0300

    Added post page and subscription validation

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -18,6 +18,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
+    console.log(session);
+
     const user = await fauna.query<User>(
       q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
     );
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,9 +3,10 @@ import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
-import { GetStaticProps } from 'next';
 
 type Post = {
   slug: string;
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}/`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
